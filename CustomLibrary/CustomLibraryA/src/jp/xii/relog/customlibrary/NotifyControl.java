eaf14ab6f1654fe0666aa64976cb35284d3c613f
iori.ayane@gmail.com <iori.ayane@gmail.com@ed0f8b31-0c64-e915-6b75-d3eca84fd226>
2011-04-10 10:02:14 +0000

    Customlibrary : NotifyControl修正。高頻度呼出を防止する機能追加。

--- CustomLibrary/CustomLibraryA/src/jp/xii/relog/customlibrary/NotifyControl.java
+++ CustomLibrary/CustomLibraryA/src/jp/xii/relog/customlibrary/NotifyControl.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
 
 public class NotifyControl {
@@ -12,19 +11,16 @@ public class NotifyControl {
 	private Activity _activity = null;
 	private int _icon_resourse_id = android.R.drawable.stat_sys_warning;
 	
+	//アップデート間隔の制御
+	private long _lastUpdateTime = 0;		//前回アップデートした時間
+	private long _updateInterval = 500;		//更新間隔(デフォルト500msec)
+	
 	//通知領域表示関係
 	private NotificationManager _notificationManager = null;	
 	private Notification _notification = null;
 	private Intent _notifyIntent = null;
 	private PendingIntent _contentIntent = null;
 
-	/**
-	 * コンテキストの取得
-	 * @return
-	 */
-	public Context getContext() {
-		return _activity;
-	}
 
 	/**
 	 * 親のアクティビティの取得
@@ -44,6 +40,21 @@ public class NotifyControl {
 
 
 	/**
+	 * 更新間隔
+	 * @param _updateInterval the _updateInterval to set
+	 */
+	public void setUpdateInterval(long _updateInterval) {
+		this._updateInterval = _updateInterval;
+	}
+	/**
+	 * 更新間隔
+	 * @return the _updateInterval
+	 */
+	public long getUpdateInterval() {
+		return _updateInterval;
+	}
+
+	/**
 	 * コンストラクタ
 	 * @param context
 	 */
@@ -52,38 +63,73 @@ public class NotifyControl {
 		setIcon(icon);
 	}
 	
+	public NotifyControl(){
+		
+	}
+	
 	/**
 	 * Notifyの初期化
 	 * @param context
 	 */
 	public void initNotify(String message){
-		
-		_notificationManager = (NotificationManager)getActivity().getSystemService(android.content.Context.NOTIFICATION_SERVICE);
-		_notification = new Notification(
-					 	_icon_resourse_id,
-						message,
-						System.currentTimeMillis());
-		_notifyIntent = getActivity().getIntent();
-		_contentIntent = PendingIntent.getActivity(getContext(), 0, _notifyIntent, 0);
+		initNotify(getActivity(), _icon_resourse_id, message);
+	}
+
+	/**
+	 * Notifyの初期化
+	 * @param activity
+	 * @param icon
+	 * @param message
+	 */
+	public void initNotify(Activity activity, int icon, String message){
+		if((activity == null) || (message == null)){
+		}else{
+			_notificationManager = (NotificationManager)activity.getSystemService(android.content.Context.NOTIFICATION_SERVICE);
+			_notification = new Notification(
+						 	icon,
+							message,
+							System.currentTimeMillis());
+			_notifyIntent = activity.getIntent();
+			_contentIntent = PendingIntent.getActivity(activity, 0, _notifyIntent, 0);
+		}
 	}
 	
 	/**
 	 * Notifyを表示する
-	 * @param context
+	 * @param title
 	 * @param message
+	 * @param app_name
 	 */
 	public void viewNotify(String title, String message, int app_name){
-
-		if(_notification == null && _notificationManager == null){
+		viewNotify(getActivity(), title, message, app_name);
+	}	
+	
+	/**
+	 * Notifyを表示する
+	 * @param activity
+	 * @param title
+	 * @param message
+	 * @param app_name
+	 */
+	public void viewNotify(Activity activity, String title, String message, int app_name){
+		long now = System.currentTimeMillis();
+		
+		if((_notification == null) || (_notificationManager == null)
+				|| (activity == null)){
+		}else if((now - _lastUpdateTime) < getUpdateInterval()){
+			//呼出が早過ぎる
 		}else{
-			_notification.when = System.currentTimeMillis();
+			_notification.when = now;
 			_notification.setLatestEventInfo(
-					getContext(),
+					activity,
 					title,
 					message,
 					_contentIntent);
 			
 			_notificationManager.notify(app_name, _notification);
+
+			//前回の更新時間を保存
+			_lastUpdateTime = now;
 		}
 	}
 
